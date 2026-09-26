acd9bdd342070b0fe6ca2cc17e502cd43c3dcc75
Ali Khatibi <ak88hb@gmail.com>
2026-09-26 17:02:30 +0330

    Updates

---
+++ assets/esm-fGjDZeTg.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BX1JdHcP.js","assets/dist-3eupxzPp.js"])))=>i.map(i=>d[i]);
+import{t as e}from"./preload-helper-DPtG3hyX.js";import{n as t}from"./dist-3eupxzPp.js";var n=t(`Browser`,{web:()=>e(()=>import(`./web-BX1JdHcP.js`).then(e=>new e.BrowserWeb),__vite__mapDeps([0,1]))});export{n as Browser};
